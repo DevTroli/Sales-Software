088957caf0750ecea5a5b964f134e3812d95feb6
Pablo Troli <brawlixo123@gmail.com>
2024-07-04 17:19:22 -0300

    django project setup

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField("Criadoi em:", auto_now_add=True, auto_now=False)
+    modified = models.DateTimeField("Modificado em", auto_now_add=False, auto_now=True)
+
+
+class Meta:
+    abstract = True
