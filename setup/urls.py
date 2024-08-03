1662304d4e77efaf7264d9fa582877a3ebbde60d
Pablo Troli <brawlixo123@gmail.com>
2024-08-03 02:41:53 -0300

    update nav.html

--- setup/urls.py
+++ setup/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("produtos/", include("produto.urls")),
     path("estoque/", include("estoque.urls")),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+
 ]
