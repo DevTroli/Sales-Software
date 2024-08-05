3579125a9047f533201feac8d8576b040565302b
Troli <119256727+DevTroli@users.noreply.github.com>
2024-08-05 06:00:23 +0000

    update views.py

--- produto/views.py
+++ produto/views.py
@@ -11,6 +11,8 @@ from django.http import HttpResponse
 
 import pandas as pd
 
+from openpyxl.styles import PatternFill, Border, Side, Alignment
+
 from produto.models import Produto
 from produto.forms import ProdutoForm
 
@@ -52,111 +54,95 @@ def product_add(request):
 
 @login_required
 def gerar_insights(request):
-    # Obter todos os produtos
-    df = pd.DataFrame(list(Produto.objects.all().values()))
+    try:
+        # Obter todos os produtos
+        df = pd.DataFrame(list(Produto.objects.all().values()))
 
-    # Processar dados
-    produtos_abaixo_estoque_minimo = df[df["estoque"] < df["estoque_minimo"]]
-    df["valor_venda_potencial"] = df["estoque"] * df["preco_venda"]
-    produtos_preco_zero = df[df["preco_venda"] == 0]
-    produtos_estoque_excedente = df[df["estoque"] > df["estoque_minimo"] * 2]
-    df["rotatividade"] = df["estoque"] / df["estoque_minimo"]
-    df["custo_estoque"] = df["estoque"] * df["preco_custo"]
-    produtos_alto_custo_estoque = df[df["custo_estoque"] > 1000]
+        # Processar dados
+        produtos_abaixo_estoque_minimo = df[df["estoque"] < df["estoque_minimo"]]
+        df["valor_venda_pot"] = df["estoque"] * df["preco_venda"]
+        produtos_preco_zero = df[df["preco_venda"] == 0]
+        produtos_estoque_excedente = df[df["estoque"] > df["estoque_minimo"] * 2]
+        df["rotatividade"] = df["estoque"] / df["estoque_minimo"]
+        df["custo_estoque"] = df["estoque"] * df["preco_custo"]
+        produtos_alto_custo_estoque = df[df["custo_estoque"] > 1000]
 
-    # Calcular o valor total das vendas
-    valor_total_vendas = (df["estoque"] * df["preco_venda"]).sum()
+        # Calcular o valor total das vendas
+        valor_total_vendas = (df["estoque"] * df["preco_venda"]).sum()
 
-    # Criar um buffer para o arquivo Excel
-    buffer = BytesIO()
-    with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
+        # Criar um buffer para o arquivo Excel
+        buffer = BytesIO()
+        with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
 
-        def style_worksheet(worksheet):
-            for row in worksheet.iter_rows(
-                min_row=2, max_col=worksheet.max_column, max_row=worksheet.max_row
-            ):
-                for cell in row:
-                    if cell.row % 2 == 0:
-                        cell.fill = PatternFill(
-                            start_color="D9EAD3", end_color="D9EAD3", fill_type="solid"
+            def style_worksheet(worksheet):
+                for row in worksheet.iter_rows(
+                    min_row=2, max_col=worksheet.max_column, max_row=worksheet.max_row
+                ):
+                    for cell in row:
+                        if cell.row % 2 == 0:
+                            cell.fill = PatternFill(
+                                start_color="D9EAD3", end_color="D9EAD3", fill_type="solid"
+                            )
+                        else:
+                            cell.fill = PatternFill(
+                                start_color="FFFFFF", end_color="FFFFFF", fill_type="solid"
+                            )
+                        cell.border = Border(
+                            left=Side(border_style="thin"),
+                            right=Side(border_style="thin"),
+                            top=Side(border_style="thin"),
+                            bottom=Side(border_style="thin"),
                         )
-                    else:
-                        cell.fill = PatternFill(
-                            start_color="FFFFFF", end_color="FFFFFF", fill_type="solid"
-                        )
-                    cell.border = Border(
-                        left=Side(border_style="thin"),
-                        right=Side(border_style="thin"),
-                        top=Side(border_style="thin"),
-                        bottom=Side(border_style="thin"),
-                    )
-                    cell.alignment = Alignment(horizontal="center", vertical="center")
+                        cell.alignment = Alignment(horizontal="center", vertical="center")
 
-        produtos_abaixo_estoque_minimo.to_excel(
-            writer, sheet_name="Produtos Abaixo do Estoque Mínimo", index=False
-        )
-        df[["produto", "valor_venda_potencial"]].to_excel(
-            writer, sheet_name="Valor Venda Potencial", index=False
-        )
-        produtos_preco_zero.to_excel(
-            writer, sheet_name="Produtos Preço Zero", index=False
-        )
-        produtos_estoque_excedente.to_excel(
-            writer, sheet_name="Produtos Estoque Excedente", index=False
-        )
-        df[["produto", "rotatividade"]].to_excel(
-            writer, sheet_name="Rotatividade Estoque", index=False
-        )
-        produtos_alto_custo_estoque.to_excel(
-            writer, sheet_name="Produtos Alto Custo Estoque", index=False
-        )
+            produtos_abaixo_estoque_minimo.to_excel(
+                writer, sheet_name="Abaixo Estoque Min", index=False
+            )
+            df[["produto", "valor_venda_pot"]].to_excel(
+                writer, sheet_name="Venda Potencial", index=False
+            )
+            produtos_preco_zero.to_excel(
+                writer, sheet_name="Produtos Zerados", index=False
+            )
+            produtos_estoque_excedente.to_excel(
+                writer, sheet_name="Estoque Excedente", index=False
+            )
+            df[["produto", "rotatividade"]].to_excel(
+                writer, sheet_name="Rotatividade", index=False
+            )
+            produtos_alto_custo_estoque.to_excel(
+                writer, sheet_name="Custo Alto Estoque", index=False
+            )
 
-        # Adicionar aba com o valor total das vendas
-        total_vendas_df = pd.DataFrame({"Valor Total Vendas": [valor_total_vendas]})
-        total_vendas_df.to_excel(writer, sheet_name="Total Vendas", index=False)
+            # Adicionar aba com o valor total das vendas
+            total_vendas_df = pd.DataFrame({"Total Vendas": [valor_total_vendas]})
+            total_vendas_df.to_excel(writer, sheet_name="Total Vendas", index=False)
 
-        # Aplicar formatação
-        for sheet_name in writer.sheets:
-            sheet = writer.sheets[sheet_name]
-            style_worksheet(sheet)
+            # Aplicar formatação
+            for sheet_name in writer.sheets:
+                sheet = writer.sheets[sheet_name]
+                style_worksheet(sheet)
 
-    buffer.seek(0)
+        buffer.seek(0)
 
-    # Criar a resposta para o download
-    response = HttpResponse(
-        buffer,
-        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    )
-    response["Content-Disposition"] = 'attachment; filename="insights_estoque.xlsx"'
+        # Criar a resposta para o download
+        response = HttpResponse(
+            buffer,
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+        response["Content-Disposition"] = 'attachment; filename="insights_estoque.xlsx"'
 
-    return response
+        return response
+
+    except Exception as e:
+        # Captura qualquer exceção e exibe a mensagem de erro
+        return HttpResponse(f"Erro ao gerar insights: {str(e)}", status=500)
 
 
 def import_csv(request):
     template_name = "import_csv.html"
     return render(request, template_name)
 
-
-def dashboard(request):
-    # Coleta de dados para o gráfico
-    produtos = Produto.objects.all()
-    vendas = [produto.valor_venda_potencial for produto in produtos]  # Exemplo de dados
-
-    # Criação do gráfico
-    fig, ax = plt.subplots()
-    ax.plot(vendas, label="Vendas Potenciais")
-    ax.set_title("Gráfico de Vendas Potenciais")
-    ax.set_xlabel("Produto")
-    ax.set_ylabel("Valor")
-
-    # Convertendo o gráfico para imagem PNG
-    buf = io.BytesIO()
-    plt.savefig(buf, format="png")
-    buf.seek(0)
-    image_data = base64.b64encode(buf.getvalue()).decode("utf-8")
-    buf.close()
-
-    return render(request, "dashboard.html", {"graph": image_data})
 
 
 class ProductCreate(LoginRequiredMixin, CreateView):
