58301cf19dcd968a32fe2f0b05390d6a6fd6b607
Troli <119256727+DevTroli@users.noreply.github.com>
2024-10-23 16:44:43 +0000

    🗄️ database backup in staging env

---
+++ setup/staging.py
@@ -0,0 +1,15 @@
+from .settings import *  # Importa todas as configurações do settings.py
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',  # nome do banco de dados em staging
+        'USER': 'postgres',  # usuário de staging
+        'PASSWORD': 'rlXzTQrPrZzlOqbTQXpZKUXxdibZuYDT',  # senha do banco de dados em staging
+        'HOST': 'autorack.proxy.rlwy.net',  # host do banco de staging
+        'PORT': '21844',  # porta do banco de staging
+    }
+}
+
+# Outras configurações específicas de staging
+DEBUG = False
