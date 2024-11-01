7f2015d3c41fd6c15d77b8bce7927775ecb85907
Pablo Troli <brawlixo123@gmail.com>
2024-11-01 08:30:20 -0300

    🔧 Update .env Generator

--- contrib/envGen.py
+++ contrib/envGen.py
@@ -3,24 +3,26 @@
 """
 Django SECRET_KEY generator.
 """
+
 from django.utils.crypto import get_random_string
 
 chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
 
-CONFIG_STRING = """
+CONFIG_STRING = (
+    """
 DEBUG=True
 SECRET_KEY=%s
 ALLOWED_HOSTS=127.0.0.1, .localhost
-#Databases
-DB_ENGINE=django.db.backends.postgresql
-DB_NAME=railway
-DB_USER=postgres
-DB_PASSWORD=eFBaLbnThPCCfUmprlxvZfugNZQiVBoY
-DB_HOST=viaduct.proxy.rlwy.net
-DB_PORT=57938
+#Databases (Add your access to database )
+DB_ENGINE=
+DB_NAME=
+DB_USER=
+DB_PASSWORD=
+DB_HOST=
+DB_PORT=
 
-""".strip() % get_random_string(
-    50, chars
+""".strip()
+    % get_random_string(50, chars)
 )
 
 # Writing our configuration file to '.env'
